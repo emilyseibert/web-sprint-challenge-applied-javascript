1dde48179c2034a39cfcc9d8a8dec981f6016b3b
Emily <emseibert@gmail.com>
2020-08-15 15:23:11 -0600

    sprint MVP

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,4 @@
+import { filterCards } from "./Cards";
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +10,25 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+const Tabs = () => {
+  axios
+    .get("https://lambda-times-api.herokuapp.com/topics")
+    .then((response) => {
+      // console.log("response", response.data.topics);
+      const parentDOMElement = document.querySelector(".topics");
+
+      response.data.topics.map((topic) => {
+        const tab = document.createElement("div");
+        tab.classList.add("tab");
+        tab.textContent = topic;
+        tab.addEventListener("click", () => {
+          tab.classList.toggle("active-tab");
+          filterCards();
+        });
+
+        parentDOMElement.appendChild(tab);
+      });
+    });
+};
+
+Tabs();
